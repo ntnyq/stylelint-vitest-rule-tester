78752d9f85d38a6e488a7503d5ebebd94706ee05
ntnyq <ntnyq13@gmail.com>
2025-03-11 13:50:08 +0800

    ci: setup autofix action

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,8 +4,8 @@ export * from './resolveRuleMeta'
 export * from './normalizeTestCase'
 export * from './resolveRuleOptions'
 export * from './validateLintResult'
-export * from './resolveLinterOptions'
 export * from './normalizeCaseMessage'
+export * from './resolveLinterOptions'
 export * from './normalizeLinterResult'
 
 export { unindent as $, unindent }
